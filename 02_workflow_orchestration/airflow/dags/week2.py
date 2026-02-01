dcfd9d8ff60f9274f0a6b9b9f59b7248514e2f9d
mushroomsandchai <ajaymkatteo.0@gmail.com>
2026-02-01 15:10:00 +0000

    Automated the entire workflow, including creating and inserting from gcp bucket directly from airflow.

--- 02_workflow_orchestration/airflow/dags/week2.py
+++ 02_workflow_orchestration/airflow/dags/week2.py
@@ -6,7 +6,7 @@ from zoneinfo import ZoneInfo
 WORK_DIR = '/tmp/week2/'
 
 def get_dates(context):
-    month, year = str(context['logical_date'].month).zfill(2), context['logical_date'].year
+    month, year = str(context['logical_date'].month).zfill(2), str(context['logical_date'].year)
     return (month, year)
 
 @dag(
@@ -47,10 +47,90 @@ def week_2_ingestion():
     def clean():
         return(f"rm -rf {WORK_DIR}")
 
+    @task
+    def big_query_insert():
+        from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+        hook = BigQueryHook()
+        client = hook.get_client()
+        month, year = get_dates(get_current_context())
+        for taxi in (['yellow', 'green']):
+            green_query = f"""
+                                create table if not exists dtdez.{taxi}_{year} (
+                                    VendorID INT64,
+                                    lpep_pickup_datetime TIMESTAMP,
+                                    lpep_dropoff_datetime TIMESTAMP,
+                                    store_and_fwd_flag BOOL,
+                                    RatecodeID INT64,
+                                    PULocationID INT64,
+                                    DOLocationID INT64,
+                                    passenger_count INT64,
+                                    trip_distance FLOAT64,
+                                    fare_amount FLOAT64,
+                                    extra FLOAT64,
+                                    mta_tax FLOAT64,
+                                    tip_amount FLOAT64,
+                                    tolls_amount FLOAT64,
+                                    ehail_fee STRING,
+                                    improvement_surcharge FLOAT64,
+                                    total_amount FLOAT64,
+                                    payment_type INT64,
+                                    trip_type INT64,
+                                    congestion_surcharge FLOAT64
+                                );
+
+                                create or replace external table dtdez.{taxi}_{year}_{month}_ext options (
+                                uris = ['gs://dtdez/{taxi}/{year}-{month}.csv'],
+                                format = 'csv'
+                                );
+
+                                insert into dtdez.{taxi}_{year}
+                                select * from dtdez.{taxi}_{year}_{month}_ext;
+
+                                drop table dtdez.{taxi}_{year}_{month}_ext;
+                            """
+
+            yellow_query = f"""create table if not exists dtdez.{taxi}_{year} (
+                                    VendorID INT64,
+                                    tpep_pickup_datetime TIMESTAMP,
+                                    tpep_dropoff_datetime TIMESTAMP,
+                                    passenger_count INT64,
+                                    trip_distance FLOAT64,
+                                    RatecodeID INT64,
+                                    store_and_fwd_flag BOOL,
+                                    PULocationID INT64,
+                                    DOLocationID INT64,
+                                    payment_type INT64,
+                                    fare_amount FLOAT64,
+                                    extra FLOAT64,
+                                    mta_tax FLOAT64,
+                                    tip_amount FLOAT64,
+                                    tolls_amount FLOAT64,
+                                    improvement_surcharge FLOAT64,
+                                    total_amount FLOAT64,
+                                    congestion_surcharge FLOAT64
+                                );
+
+                                create or replace external table dtdez.{taxi}_{year}_{month}_ext options (
+                                uris = ['gs://dtdez/{taxi}/{year}-{month}.csv'],
+                                format = 'csv'
+                                );
+
+                                insert into dtdez.{taxi}_{year}
+                                select * from dtdez.{taxi}_{year}_{month}_ext;
+
+                                drop table dtdez.{taxi}_{year}_{month}_ext;
+                             """
+            if taxi == 'yellow':
+                if year == '2020' or (year == '2021' and month == '03'):
+                    query_job = client.query(yellow_query)
+            elif year == '2020':
+                query_job = client.query(green_query)
+
     ingester = ingest()
     loader = load()
     cleaner = clean()
+    inserter = big_query_insert()
 
-    ingester >> loader >> cleaner
+    ingester >> loader >> cleaner >> inserter
 
 week_2_ingestion()
--- 02_workflow_orchestration/bq.sql
+++ 02_workflow_orchestration/bq.sql
@@ -1,39 +1,12 @@
-CREATE OR REPLACE EXTERNAL TABLE `project_id.dataset.2020_y`
-OPTIONS (
-  format = 'csv',
-  uris = ['gs://bucket/yellow/2020*']
-);
-
-CREATE OR REPLACE EXTERNAL TABLE `project_id.dataset.2021_y`
-OPTIONS (
-  format = 'csv',
-  uris = ['gs://bucket/yellow/2021-03*']
-);
-
-CREATE OR REPLACE EXTERNAL TABLE `project_id.dataset.2020_g`
-OPTIONS (
-  format = 'csv',
-  uris = ['gs://bucket/green/2020*']
-);
-
-CREATE OR REPLACE EXTERNAL TABLE `project_id.dataset.2021_g`
-OPTIONS (
-  format = 'csv',
-  uris = ['gs://bucket/green/2021*']
-);
-
-SELECT "2020_green"  AS dataset, COUNT(*) AS record_count FROM `project_id.dataset.2020_g`
-UNION ALL
-SELECT "2021_green",  COUNT(*) FROM `project_id.dataset.2021_g`
-UNION ALL
-SELECT "2020_yellow", COUNT(*) FROM `project_id.dataset.2020_y`
-UNION ALL
-SELECT "2021_yellow", COUNT(*) FROM `project_id.dataset.2021_y`;
+select '2020_green', count(*) from `project_id.dataset.green_2020`
+union all
+select '2020_yellow', count(*) from `project_id.dataset.yellow_2020`
+union all
+select '2021_yellow', count(*) from `project_id.dataset.yellow_2021`;
 
 -- Expected results:
 -- | dataset        | record_count |
 -- | -------------- | ------------ |
--- | 2021_green     | 570,466      |
 -- | 2020_green     | 1,734,051    |
 -- | 2021_yellow    | 1,925,152    |
 -- | 2020_yellow    | 24,648,499   |
